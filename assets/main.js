c01df545bf70aef21ffb3661931fa7636a1db61c
Jacob Packert <jacob.packert@mediacom.dk>
2016-12-10 17:19:31 +0100

    removed console.log

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,15 @@
 $(function() {
 
   // your code will go here
-$.ajax({url: 'https://www.codeschool.com/users/jacobpackert.json',
+$.ajax({
+  url: 'https://www.codeschool.com/users/jacobpackert.json',
   dataType: 'jsonp',
   succes: function(response){
-        console.log('success')
-  }
+
+      }
 
 
-});
-
+  });
 
 
 
