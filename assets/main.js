2017466d5f7bedd9a3a15491f75d0f9ebfcdef5d
Jacob Packert <jacob.packert@mediacom.dk>
2016-12-10 17:07:41 +0100

    added AJAX call to my report card

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
   // your code will go here
+$.ajax({url: 'https://www.codeschool.com/users/jacobpackert.json',
+  dataType: 'jsonp',
+  succes: function(response){
+        console.log('success')
+  }
+
+
+});
+
+
+
 
 });
