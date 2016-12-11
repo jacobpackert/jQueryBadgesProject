356f1b3f4e1117bbc6d857cc72b73951b154bcce
Jacob Packert <jacob.packert@mediacom.dk>
2016-12-11 11:15:25 +0100

    adding div-adding functions to #badges

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,22 @@ $.ajax({
   url: 'https://www.codeschool.com/users/jacobpackert.json',
   dataType: 'jsonp',
   success: function(response){
+    // console.log(response);
+    $.each(response.courses.completed, function(i){
+      $('#badges').append('<div class="course"></div>');
+      });
+
+
+
+// I can't get this to work yet
+//     $.map(response, function(i){
+//       // console.log("mapping");
+//       var list = $('<div class="course">' + i + '</div>');
+//       $('<p>' + response.courses.completed + '</p>').appendTo(list);
+//       console.log(list);
+// })
+
+
 
       }
 
