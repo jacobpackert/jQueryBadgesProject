544f95372231755bbb0d2b97c5b64dc24f4a4ffc
Jacob Packert <jacob.packert@mediacom.dk>
2016-12-11 16:34:24 +0100

    adding links to courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,14 +7,28 @@ $.ajax({
   success: function(response){
     console.log(response);
     $.each(response.courses.completed, function(i){
+      // generate the item that we're gonna fill stuff into
       var item = $('<div class="course"></div>');
+
+      // generate the course title
       var courseTitle = response.courses.completed[i].title;
       $('<h3>' + courseTitle + '</h3>').appendTo(item);
+
+      // generate the coursebadge
       var courseBadge = response.courses.completed[i].badge;
       var badgeImage = $('<img></img>');
       $(badgeImage).attr("src", courseBadge);
-      // console.log(badgeImage);
       $(badgeImage).appendTo(item);
+
+      // generate the link
+      var courseLink = $('<a>See Course</a>');
+      $(courseLink).attr({
+        "href": response.courses.completed[i].url,
+        "target": '_blank',
+        "class": 'btn btn-primary'
+      });
+      $(courseLink).appendTo(item);
+      // append it all to badges
       $('#badges').append(item);
       });
 
